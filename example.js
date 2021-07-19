24a892e638f92c708a802b61b2bf21649578180f
Sandeep Kumar <ki.sandeep11@gmail.com>
2021-07-19 12:28:55 +0530

    Added file driver support

--- example.js
+++ example.js
@@ -3,20 +3,37 @@ const express = require('express');
 const app = express();
 const http = require('http');
 
-const { getTokenId, getToken, getTokenHeader, setToken, updateToken, deleteToken, refreshTokenId, extendTokenTime } = require('nodejs-token-auth');
+const {
+    getTokenId,
+    getToken,
+    getTokenHeader,
+    setToken,
+    updateToken,
+    deleteToken,
+    refreshTokenId,
+    extendTokenTime,
+    setTokenSavePath,
+    getTokenSavePath,
+    tokenDriver
+} = require('nodejs-token-auth');
 
 app.get('/', (req, res) => {
+    tokenDriver('memory')
     const tokenId = getTokenId();
     setToken(tokenId, { id: 123, name: "user1" })
-    updateToken(tokenId, { email: 'user4@mail.com' })
+    updateToken(tokenId, { email: 'user5@mail.com' })
 
-    let newTokenId = refreshTokenId(tokenId)
+    // let newTokenId = refreshTokenId(tokenId)
     extendTokenTime(tokenId);
-    const tokenBody = getToken(newTokenId)
-    const tokenHeader = getTokenHeader(newTokenId)
-    deleteToken(tokenId)
+    const tokenBody = getToken(tokenId)
+    const tokenHeader = getTokenHeader(tokenId)
+    // const deleteT = deleteToken(newTokenId)
 
-    res.json([tokenHeader, tokenBody, tokenId]);
+    // setTokenSavePath('/temp-token')
+    // const path = getTokenSavePath()
+    // const tokenId = getTokenId();
+    // console.log(path)
+    res.json([tokenId, tokenBody, tokenHeader]);
 })
 
 const httpServer = http.createServer(app);
--- index.js
+++ index.js
@@ -1,31 +1,143 @@
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
 
-let tokenList = new Map();
+let tokenList = new Map()
+
+let config = {
+    tokenSavePath: './tokens',
+    tokenDriver: 'memory',
+    maxAge: 3600 * 24 * 365,
+    garbageCleanInterval: 3600,
+    tempTokenCleanAt: 600
+}
+
+function _getTokenData(key) {
+
+    if (config.tokenDriver === 'memory') {
+        return tokenList.get(key)
+    }
+    else if (config.tokenDriver === 'file') {
+        let token = fs.readFileSync(getTokenSavePath() + '/' + key)
+        return JSON.parse(token)
+    }
+    else if (config.tokenDriver === 'database') {
+
+    }
+    else if (config.tokenDriver === 'redis') {
+
+    }
+}
+
+function _setTokenData(key, obj) {
+    try {
+        if (config.tokenDriver === 'memory') {
+            tokenList.set(key, obj)
+        }
+        else if (config.tokenDriver === 'file') {
+            fs.writeFileSync(getTokenSavePath() + '/' + key, JSON.stringify(obj, null, 2))
+        }
+        else if (config.tokenDriver === 'database') {
+
+        }
+        else if (config.tokenDriver === 'redis') {
+
+        }
+    }
+    catch (err) {
+        return
+    }
+}
+
+function _deleteTokenData(key) {
+    try {
+        if (config.tokenDriver === 'memory') {
+            tokenList.delete(key)
+        }
+        else if (config.tokenDriver === 'file') {
+            fs.unlinkSync(getTokenSavePath() + '/' + key)
+        }
+        else if (config.tokenDriver === 'database') {
+
+        }
+        else if (config.tokenDriver === 'redis') {
+
+        }
+        return true
+    }
+    catch (err) {
+        return false
+    }
+}
+
+function _getAllTokenList() {
+    try {
+        if (config.tokenDriver === 'memory') {
+            return Array.from(tokenList.keys())
+        }
+        else if (config.tokenDriver === 'file') {
+            return fs.readdirSync(getTokenSavePath() + '/')
+        }
+        else if (config.tokenDriver === 'database') {
+
+        }
+        else if (config.tokenDriver === 'redis') {
+
+        }
+    }
+    catch (err) {
+        return []
+    }
+
+}
+
+function tokenDriver(driver) {
+    if (driver === 'memory' || driver === 'file' || driver === 'database' || driver === 'redis') {
+        config.tokenDriver = driver
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+function setTokenSavePath(path) {
+    config.tokenSavePath = path
+}
+
+function getTokenSavePath() {
+    return config.tokenSavePath
+}
 
 function getTokenId() {
-    let token = Buffer.from(uuidv4()).toString('base64')
-    tokenList.set(token, { header: { maxAge: 3600*24*365, createdAt: new Date().getTime() }, body: {} })
-    return token
+    let payload = { header: { maxAge: config.maxAge, createdAt: new Date().getTime() }, body: {} }
+    let tokenId = Buffer.from(uuidv4()).toString('base64')
+    _setTokenData(tokenId, payload)
+    return tokenId
 }
 
 function setToken(key, obj) {
-    let token = tokenList.get(key)
-    tokenList.set(key, { header: token.header, body: Object.assign(token.body || {}, obj) })
+    try {
+        let token = _getTokenData(key)
+        let payload = { header: token.header, body: Object.assign(token.body || {}, obj) }
+        _setTokenData(key, payload)
+    }
+    catch (err) {
+        return
+    }
 }
 
 function getTokenHeader(key) {
     try {
-        return tokenList.get(key).header
+        return _getTokenData(key).header
     }
     catch (err) {
         return null
     }
-
 }
 
 function getToken(key) {
     try {
-        return tokenList.get(key).body
+        return _getTokenData(key).body
     }
     catch (err) {
         return null
@@ -34,8 +146,9 @@ function getToken(key) {
 
 function updateToken(key, obj) {
     try {
-        let token = tokenList.get(key)
-        tokenList.set(key, { header: token.header, body: Object.assign(token.body || {}, obj) })
+        let token = _getTokenData(key)
+        let payload = { header: token.header, body: Object.assign(token.body || {}, obj) }
+        _setTokenData(key, payload)
     }
     catch (err) {
         return null
@@ -44,7 +157,7 @@ function updateToken(key, obj) {
 
 function deleteToken(key) {
     try {
-        tokenList.delete(key)
+        _deleteTokenData(key)
         return true
     }
     catch (err) {
@@ -52,16 +165,12 @@ function deleteToken(key) {
     }
 }
 
-function deleteAllToken() {
-    tokenList = new Map()
-}
-
 function refreshTokenId(key) {
     try {
-        let prevToken = tokenList.get(key)
+        let prevToken = _getTokenData(key)
         let newTokenId = getTokenId()
         deleteToken(key)
-        tokenList.set(newTokenId, prevToken)
+        _setTokenData(newTokenId, prevToken)
         extendTokenTime(newTokenId)
         return newTokenId;
     }
@@ -72,8 +181,9 @@ function refreshTokenId(key) {
 
 function extendTokenTime(key) {
     try {
-        let token = tokenList.get(key)
-        tokenList.set(key, { header: Object.assign(token.header, { createdAt: new Date().getTime() }), body: token.body })
+        let token = _getTokenData(key)
+        let payload = { header: Object.assign(token.header, { createdAt: new Date().getTime() }), body: token.body }
+        _setTokenData(key, payload)
     }
     catch (err) {
         return null
@@ -81,12 +191,12 @@ function extendTokenTime(key) {
 }
 
 function cleanGarbageToken() {
-    tokenList.forEach((value, key) => {
-        let token = tokenList.get(key)
+    _getAllTokenList().forEach((key) => {
+        let token = _getTokenData(key)
         let tokenCreationTime = token.header.createdAt
         let currentTime = new Date().getTime()
 
-        if (JSON.stringify(token.body) === JSON.stringify({}) && currentTime > tokenCreationTime + 600 * 1000) {
+        if (JSON.stringify(token.body) === JSON.stringify({}) && currentTime > tokenCreationTime + config.tempTokenCleanAt * 1000) {
             deleteToken(key)
         }
         else if (currentTime > tokenCreationTime + token.header.maxAge * 1000) {
@@ -97,7 +207,7 @@ function cleanGarbageToken() {
 
 setInterval(() => {
     cleanGarbageToken()
-}, 1000 * 3600 * 1)
+}, config.garbageCleanInterval)
 
 module.exports = {
     getTokenId,
@@ -105,8 +215,10 @@ module.exports = {
     getToken,
     updateToken,
     deleteToken,
-    deleteAllToken,
     refreshTokenId,
     getTokenHeader,
-    extendTokenTime
+    extendTokenTime,
+    setTokenSavePath,
+    getTokenSavePath,
+    tokenDriver
 }
